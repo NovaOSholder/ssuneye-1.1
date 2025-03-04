a878690d8a925cb7d1a02c2f7e3c2e8a40ffcb51
NovaOSholder <bennuribaykondu5@gmail.com>
2025-03-04 15:21:47 +0100

    Add files via upload

---
+++ brainAuth32/happy.py
@@ -0,0 +1,15 @@
+while True:
+    cmd = input("YOUR QUESTION: ")
+
+    if cmd.lower() == "exit":
+        print("Goodbye!")
+        break
+
+    elif cmd == "Hello":
+        print("Hello! What brings you here today?")
+
+    elif cmd == "What is your name":
+        print("My name is SSuneye or Suneye!")
+    
+    else:
+        print("I don't know that because this is beta version of happy mood! :)")
